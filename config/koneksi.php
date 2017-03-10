5e672c6867c19a3d4cea38de0144e37498a8686b
RianAsmara <gagahkalem@gmail.com>
2017-03-10 16:31:43 +0800

    PendataanSkripsiUpdate2

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,4 +1,8 @@
 <?php
-//$host="localhost"; //$user="root"; //$pass=''; //$database="db_echy";
-$connection = mysqli_connect("localhost","root","","db_echy");
+$host="localhost"; 
+$user="root"; 
+$pass=''; 
+$database="db_echy";
+//$connection = mysqli_connect("localhost","root","","db_echy") or die (mysqli_error());
+
 ?>
